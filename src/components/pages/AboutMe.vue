6a1f9b4f5963032e9072112b95bc65481642d447
osmn-byhn <osmanbeyhan12@gmail.com>
2023-04-16 01:11:43 +0300

    commit for AboutMe.vue

--- src/components/pages/AboutMe.vue
+++ src/components/pages/AboutMe.vue
@@ -1,35 +1,6 @@
 <script setup>
     const date = new Date();
     let year = date.getFullYear();
-
-
-    // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQCILfIMQB6ujvZl4tKF95jEqTNvao1_Y0ocO6A22tKZvSQEBfSDZSO3UT9npkZPQgSunicz8Hul-Bkgz1NKy2Pph7BWFCQ44qUW9159F5gtYan0bYqlhRvf7F9Fmx6XlucPipT3BJJFWI-vosnl0no3jFI_Eo6kk8XGhIMXgj2Ri-bkgOuWVVIxKh2OeKSzpW7PuvQpuy7LNL4sVin_0nviyPMaqaYqM-vrswHrlR2_wg7W-looA-0cNMMg8EGmtF54HH-CGIQ4WyP36Dk_Cp_Wv49J65xD8hl9XjEijU65uCkqXtH5r3MsFrAOvFT3FZRuuiDEOuInvCdr_2NtiZVMqZZtdnKlKSpyQN8kEDqU-VUEM6s';
-async function fetchWebApi(endpoint, method, body) {
-  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method,
-    body:JSON.stringify(body)
-  });
-  return await res.json();
-}
-
-async function getTopTracks(){
-  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
-  )).items;
-}
-
-const topTracks = await getTopTracks();
-console.log(
-  topTracks?.map(
-    ({name, artists}) =>
-      `${name} by ${artists.map(artist => artist.name).join(', ')}`
-  )
-);
 </script>
 
 <template>
